7e36adaf09579125ee174319fe2ac9dfbd099eea
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-05-23 18:31:57 -0400

    chore: add handler book store

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -2,7 +2,12 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/mrojasb2000/go-bookstore/pkg/controllers"
 )
 
 var RegisterBooStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 }
